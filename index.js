25add0141a6537f442c660f5e949668d87299d45
Ronny Luna <luna.ronny.barria@gmail.com>
2024-06-04 18:45:32 -0400

    pre render 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { saveAllLogs, readLogsThaemine, readLogsVoldis, readLogsAkkan } = require('./crud');
 const multer = require('multer');
 const cors = require('cors');
-const upload = multer({ dest: '/api/logs/' });
+const upload = multer({ dest: 'logs/' });
 const path = require('path');
 const app = express();
 
