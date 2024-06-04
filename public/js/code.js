c968afe6dde0a307f6925f701cefedeb957a8807
papo <jo.papoh@gmail.com>
2024-06-03 21:46:11 -0400

    handling radio

--- public/js/code.js
+++ public/js/code.js
@@ -7,36 +7,6 @@ let btns = document.querySelectorAll('input');
 var rows;
 let resultados = '';
 
-function getLogs() {
-    let raid = document.querySelector('input[name="btnRaid"]:checked').value;
-    let difficulty = document.querySelector('input[name="btnDifficulty"]:checked').value;
-    let id = document.querySelector('input[name="btnGate"]:checked').value;
-    let dmg = document.querySelector('input[name="btnDmgDealt"]:checked').value;
-    let btns = document.querySelectorAll('input');
-    let gate4 = document.getElementsByName('gate4');
-    let hardest = document.getElementsByName('hardest');
-
-    if (raid === 'voldis') {
-        gate4[0].style.display = "block";
-    } else {
-        gate4[0].style.display = "none";
-    }
-    if (raid === 'thaemine') {
-        hardest[0].style.display = "none";
-    } else {
-        hardest[0].style.display = "block";
-    }
-    btns.forEach((btn) => { btn.disabled = true });
-    resultados = '';
-    contenedor.innerHTML = resultados;
-    debugger
-    fetch(url + raid + '/' + difficulty + '/' + id + '/' + dmg)
-        .then(response => response.json())
-        .then(data => mostrar(data))
-        .catch(error => console.log(error));
-}
-
-
 searchInput.addEventListener('keyup', function (event) {
     const q = event.target.value.toLowerCase();
     rows.forEach((row) => {
@@ -101,39 +71,54 @@ const mostrar = (players) => {
     rows = document.querySelectorAll('tbody tr');
 };
 
-let id = '/1';
+var hard = document.getElementById('difficulty2');
+var gate4 = document.getElementById('gate4');
+var banner = document.getElementById('banner');
+let id = '3';
 let raid = 'akkan';
-let difficulty = '/Hard';
-let dmg = '/0';
+let difficulty = 'Hard';
+let dmg = '0';
 
-fetch(url + raid + difficulty + id + dmg)
-    .then(response => response.json())
-    .then(data => mostrar(data))
-    .catch(error => console.log(error));
+fetch(url + raid + '/' + difficulty + '/' + id + '/' + dmg)
+        .then(response => response.json())
+        .then(data => mostrar(data))
+        .catch(error => console.log(error));
 
 function handleRadio(radio, type){
-    if(type === 'raid'){
+    if(type === 'raid'){    
+        document.getElementById('btnGate3').checked = true;
         raid = radio.value;
+        id = 3;
+        gate4.hidden = true;        
+        hard.hidden = false;
+
         if(raid === 'akkan'){
-            document.getElementById('banner').style.backgroundImage="url(/akkan.jpg)";
+            banner.style.backgroundImage="url(/akkan.jpg)";
         }
         else if(raid === 'voldis'){
-            document.getElementById('banner').style.backgroundImage="url(/voldis.png)";
+            document.getElementById('btnGate4').checked = true;
+            banner.style.backgroundImage="url(/voldis.png)";
+            gate4.hidden = false;
+            id = 4;
         }
         else if(raid === 'thaemine'){
-            document.getElementById('banner').style.backgroundImage="url(/thaemine.jpg)";
+            document.getElementById('btnDifficulty1').checked = true;
+            banner.style.backgroundImage="url(/thaemine.jpg)";
+            hard.hidden = true;
+            difficulty = 'Normal';
         }
     }
     else if(type === 'difficulty'){
-        difficulty = radio.value || '/Normal';
+        difficulty = radio.value;
     }
     else if(type === 'gate'){
-        id = radio.value || '/1';
+        id = radio.value;
     }
     else if(type === 'dmg'){
-        dmg = radio.value || '/0';
+        dmg = radio.value;
     }
-    fetch(url + raid + difficulty + id + dmg)
+    contenedor.innerHTML = '';
+    fetch(url + raid + '/' + difficulty + '/' + id + '/' + dmg)
         .then(response => response.json())
         .then(data => mostrar(data))
         .catch(error => console.log(error));
